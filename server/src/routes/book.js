309df599a94257ed3f289bd32496d80fb54ed24b
kobyjang <java.koby@gmail.com>
2021-10-25 12:55:41 +0900

    books123

--- server/src/routes/book.js
+++ server/src/routes/book.js
@@ -40,7 +40,6 @@ BookRouter.post('/', (req, res)=> {
                     } 
                 })
              })
-   
 BookRouter.put('/:id', (req, res)=> {
 //  데이터베이스 접속 후 id 값으로 모델 죄호 후 변경됨
     // res.send(`book ${req.params.id} updated`)
